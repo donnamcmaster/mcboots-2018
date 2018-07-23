da580b99f8c13c7b3ae6c01fecb254c9c42141d7
Donna McMaster <donna@donnamcmaster.com>
2018-07-22 17:18:32 -0700

    update head meta
    - add parameter to "viewport" meta per Bootstrap 4 docs
    - remove http-equiv meta used for ancient IE
    - add link rel="profile" because Twenty Seventeen does it ;)

--- template-parts/head.php
+++ template-parts/head.php
@@ -8,7 +8,8 @@
 
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
 	<?php wp_head(); ?>
 </head>
