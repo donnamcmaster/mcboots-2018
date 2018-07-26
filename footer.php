da05d90e6e1a2efbef1ea6c1b9503f7f5c19349c
Donna McMaster <donna@donnamcmaster.com>
2018-07-25 19:41:44 -0700

    update header, nav, for bootstrap 4
    - replace previous nav walker with WP_Bootstrap_Navwalker (https://github.com/wp-bootstrap/wp-bootstrap-navwalker)
    - modify header & footer menus to use new nav walker
    - replace old hamburger construction code with new navbar-toggler

--- footer.php
+++ footer.php
@@ -9,13 +9,22 @@
  * @package McBoots-2018
  * @since 0.1
  */
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+$footer_nav_args = [
+	'theme_location' => 'footer_menu', 
+	'menu_class' => 'nav justify-content-center mt-1 mb-2',
+	'depth' => 1, // 1 = no dropdowns, 2 = with dropdowns.
+	'walker' => new WP_Bootstrap_Navwalker(),
+];
 ?>
 
 <footer class="content-info" role="contentinfo">
 	<div class="site-info">
 		<?php if ( has_nav_menu( 'footer_menu' ) ) : ?>
-			<?php wp_nav_menu( array( 'theme_location' => 'footer_menu', 'menu' => 'Footer Menu', 'menu_class' => 'nav-footer' ) ); ?>
+			<?php wp_nav_menu( $footer_nav_args ); ?>
 		<?php endif; ?>
-		<p class="copyright">&copy; <?= bloginfo( 'name' ); ?> <?php echo date('Y'); ?></p>
+		<p class="copyright">&copy; <?= bloginfo( 'name' ); ?> <?php echo date('Y'); ?> <?= edit_post_link();?></p>
 	</div><!-- .site-info -->
 </footer><!-- .content-info -->
--- header.php
+++ header.php
@@ -9,31 +9,26 @@
  * @package McBoots-2018
  * @since 0.1
  */
-?>
 
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
-	<div class="navbar-header">
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="primary-menu" aria-expanded="false">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+$primary_nav_args = [
+	'theme_location' => 'primary_navigation', 
+	'menu_class' => 'navbar-nav mr-auto mt-2 mt-lg-0',
+	'depth' => 2, // 1 = no dropdowns, 2 = with dropdowns.
+	'walker' => new WP_Bootstrap_Navwalker(),
+];
+
+?>
+<header class="banner" role="banner">
+	<nav class="navbar navbar-expand-md navbar-light bg-light">
 		<a class="navbar-brand" href="<?= home_url(); ?>/" rel="home"><?= bloginfo( 'name' ); ?></a>
-<?php
-	if ( has_nav_menu( 'social_media' ) ) {
-		wp_nav_menu( array( 'theme_location' => 'social_media', 'menu_class' => 'menu-social header' ) );
-	}
-?>
-	</div>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="primary-menu" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-<?php
-	if ( has_nav_menu( 'primary_navigation' ) ) {
-?>
-	<nav class="collapse navbar-collapse" role="navigation">
-		<?php wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'] ); ?>
+		<div class="collapse navbar-collapse" role="navigation">
+			<?php wp_nav_menu( $primary_nav_args ); ?>
+		</div>
 	</nav>
-<?php
-	}
-?>
 </header>
