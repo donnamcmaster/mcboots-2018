9223478d28725cd60794268111c917ad6dfaf27e
Donna McMaster <donna@donnamcmaster.com>
2018-10-11 18:28:17 -0700

    add utility function to streamline media lib display

--- lib/setup/admin.php
+++ lib/setup/admin.php
@@ -8,7 +8,7 @@
  * @since 0.1
  */
 
-if (!defined('ABSPATH')) exit; // Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 add_filter( 'admin_init', function () {
 	// block access to /wp-admin if current user can't edit 
@@ -18,4 +18,14 @@ add_filter( 'admin_init', function () {
         exit;
     }
 });
-	
+
+add_action( 'admin_head', function () {
+// get rid of empty space next to media quick edit menu
+?>
+<style>
+	#posts-filter table.media .column-title .has-media-icon~.row-actions {
+		margin-left: 0;
+	}
+</style>
+<?php
+});
