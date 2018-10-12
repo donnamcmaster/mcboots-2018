793580dd9c63248e940c2c4686bc8c3b1afc11b4
Donna McMaster <donna@donnamcmaster.com>
2018-10-11 18:30:38 -0700

    support different template-parts/page-header files
    for different post types

--- single.php
+++ single.php
@@ -8,7 +8,9 @@
 
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
+$post_type = get_post_type();
+
 while ( have_posts() ) : the_post();
-	get_template_part( 'template-parts/page', 'header' );
-	get_template_part( 'template-parts/content-single', get_post_type() );
+	get_template_part( 'template-parts/page-header', $post_type );
+	get_template_part( 'template-parts/content-single', $post_type );
 endwhile;
